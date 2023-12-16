ecfbd005b3444a780ca0bc12b668a5bafc1d9888
Endleaf <endleaf12@gmail.com>
2023-12-15 17:18:44 -0800

    fixed color issues, button position, and tab borders

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,30 +1,30 @@
 import { useState } from 'react';
 import './Tabs.css';
 
-export function Tabs({ tabNames, onTabSelect }: { tabNames: Array<string>, onTabSelect: Function }) {
-
+export function Tabs({ tabNames, onTabSelect }: { tabNames: Array<string>; onTabSelect: Function }) {
     const [toggleState, setToggleState] = useState(0);
 
     const toggleTab = (index: number) => {
         setToggleState(index);
         onTabSelect(index);
-    }
+    };
 
     return (
-        <div className='Tabs-tabs-container'>
-            <div className='Tabs-edge' />
-            <div className='Tabs-tabs-wrapper'>
-                {tabNames.map((tabName, i) =>
+        <div className="Tabs-tabs-container">
+            <div className="Tabs-edge" />
+            <div className="Tabs-tabs-wrapper">
+                {tabNames.map((tabName, i) => (
                     <div
                         key={i}
                         className={toggleState === i ? 'Tabs-tabs Tabs-active-tab' : 'Tabs-tabs'}
                         onClick={() => toggleTab(i)}
                     >
                         {tabName}
+                        <div className={toggleState === i ? 'Tabs-cover Tabs-active-cover' : 'Tabs-cover'} />
                     </div>
-                )}
+                ))}
             </div>
-            <div className='Tabs-edge' />
+            <div className="Tabs-edge" />
         </div>
-    )
+    );
 }
